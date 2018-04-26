14e6abc08a099c179570b63d05acdf0f1b4dbf62
Hugo Escandon <hescandon6@gmail.com>
2018-04-26 19:34:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,6 +27,10 @@ function makeInt(n) {
   parseInt('n', 10)
   return n
 }
+function makeInt(n){
+  parseInt('nonsense!', 10)
+  return n
+}
 function preserveDecimal(n){
   parseFloat(n)
   return n
